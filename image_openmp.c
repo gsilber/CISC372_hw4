6b9bf54236ccb3f2288d858558e8b342f1351917
Ella <ellawlk@udel.edu>
2023-10-30 23:28:33 -0400

    did everything

--- image_openmp.c
+++ image_openmp.c
@@ -3,15 +3,12 @@
 #include <time.h>
 #include <string.h>
 #include "image.h"
-#include <pthread.h>
-
+#include <omp.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-
-#define thread_count 10
 
 //An array of kernel matrices to be used for image convolution.  
 //The indexes of these match the enumeration from the header file. ie. algorithms[BLUR] returns the kernel corresponding to a box blur.
@@ -23,13 +20,6 @@ Matrix algorithms[]={
     {{-2,-1,0},{-1,1,1},{0,1,2}},
     {{0,0,0},{0,1,0},{0,0,0}}
 };
-
-typedef struct{
-    Image *srcImage;
-    Image *destImage;
-    enum KernelTypes algorithm;
-    long rank;
-} thread_args;
 
 
 //getPixelValue - Computes the value of a specific pixel on a specific channel using the selected convolution kernel
@@ -69,40 +59,11 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int row,pix,bit,span;
     span=srcImage->bpp*srcImage->bpp;
+    #pragma omp for
     for (row=0;row<srcImage->height;row++){
         for (pix=0;pix<srcImage->width;pix++){
             for (bit=0;bit<srcImage->bpp;bit++){
                 destImage->data[Index(pix,row,srcImage->width,bit,srcImage->bpp)]=getPixelValue(srcImage,pix,row,bit,algorithm);
-            }
-        }
-    }
-}
-
-void *thread_convolute(void *args){
-    thread_args *threadArgs = (thread_args *)args;
-    Image *srcImage = threadArgs->srcImage;
-    Image *destImage = threadArgs->destImage;
-    enum KernelTypes algorithm = threadArgs->algorithm;
-    long rank = threadArgs->rank;
-    int height = srcImage->height;
-    int rows = height / thread_count;
-
-    int row,pixel,bit,span,start,end;
-    start = rank*rows;
-    if(((rows) * (rank+1)) < height){
-        end = (rows) * (rank+1);
-    } else{
-        end = height;
-    }
-
-    int bpp = srcImage->bpp;
-    int width = srcImage->width;
-    span = bpp*bpp;
-    int index;
-    for(index=start; index < end; index++){
-        for(pixel=0;pixel<width;pixel++){
-            for(bit=0; bit < bpp; bit++){
-                destImage->data[Index(pixel,index,width,bit,bpp)] = getPixelValue(srcImage,pixel,index,bit,algorithms[algorithm]);
             }
         }
     }
@@ -151,25 +112,8 @@ int main(int argc,char** argv){
     destImage.height=srcImage.height;
     destImage.width=srcImage.width;
     destImage.data=malloc(sizeof(uint8_t)*destImage.width*destImage.bpp*destImage.height);
-
-    pthread_t *thread_handles;
-    thread_args threadArgs[thread_count];
-    thread_handles = (pthread_t *)malloc(thread_count * sizeof(pthread_t));
-
-    int thread;
-    for(thread=0;thread<thread_count;thread++){
-        threadArgs[thread].srcImage = &srcImage;
-        threadArgs[thread].destImage = &destImage;
-        threadArgs[thread].algorithm = type;
-        threadArgs[thread].rank = thread;
-        pthread_create(&thread_handles[thread], NULL, &thread_convolute,&threadArgs[thread]);
-    }
-
-    for(thread=0;thread<thread_count;thread++){
-        pthread_join(thread_handles[thread], NULL);
-    }
-
-    // convolute(&srcImage,&destImage,algorithms[type]);
+    #pragma omp parallel
+    convolute(&srcImage,&destImage,algorithms[type]);
     stbi_write_png("output.png",destImage.width,destImage.height,destImage.bpp,destImage.data,destImage.bpp*destImage.width);
     stbi_image_free(srcImage.data);
     
--- makefile
+++ makefile
@@ -1,5 +1,6 @@
-
 image:image.c image.h
-	gcc -g image.c -o image -lm
+	gcc -g image.c -o image -lm -lpthread
+imagemp: image_openmp.c -o imagemp -lm -fopenmp
+	gcc -g image_openmp.c -o imagemp -lm -fopenmp
 clean:
 	rm -f image output.png
